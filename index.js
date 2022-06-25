f60e09597012dbf563ca5c0f62ca167d00577bdb
FIGUREX <kennethggm12@gmail.com>
2022-06-25 16:11:46 -0600

    i added the form

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import { canvas } from './src/canvas.js';
 !(function () {
 	canvas();
 })();
+
+const $form = document.querySelector('#form');
+const $buttonMailto = document.querySelector('#turcazo');
+
+$form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const form = new FormData($form);
+	$buttonMailto.setAttribute(
+		'href',
+		`mailto:kenhamax20@gmail.com?
+		subject=${form.get('name')}${form.get('topic')}
+		&body=${form.get('message')}`
+	);
+	$buttonMailto.click();
+});
